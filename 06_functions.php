389eef5bdd1ae4da01b0544264436dd18856cbcb
Mary Kate Anecito <marykate09@protonmail.com>
2024-02-10 20:59:47 +0800

    Added the syntax of arrow function in 06_functions.php

--- 06_functions.php
+++ 06_functions.php
@@ -20,10 +20,14 @@
  * default - you give a default value so that if you forgot to put
  * an argument into your function, it would use the default value declared
  * in a parameter
- * anonymous
+ * anonymous - it is not a named function like function calculateArea() -
+ * calculateArea() is the name of the function
+ *  Java also have this implementation
+ * arrow -
+ *  Java also have this implementation, but it is called lambda expression
  */
 function registerUser($email) { // $email is the parameter
-    echo $email .' registered';
+    echo $email .' registered' . '<br>';
 }
 
 $emeraldEmail = 'emeraldgreenleaf@quantumtech.com';
@@ -34,22 +38,31 @@ registerUser($emeraldEmail); // Call the function to run the code inside
 // Return vs Void vs Default vs Anonymous
 // Return
 function sum($n1, $n2) {
-    return $n1 + $n2;
+    return $n1 + $n2 . '<br>';
 }
-echo sum(5, 10);
+echo sum(5, 10); // 15
 $total = sum(23, 10);
-echo $total;
+echo $total; // 33
+
 // Void
 function subtract($n1, $n2) {
-    echo $n1 - $n2;
+    echo $n1 - $n2 . '<br>';
 }
-subtract(10, 4);
+subtract(10, 4); // 6
 
 // Default
 function multiply($n1 = 0, $n2 = 0) {
-    return $n1 * $n2;
+    return $n1 * $n2 . '<br>';
 }
 echo multiply(); // 0
-echo multiply(2, 5);
+echo multiply(2, 5); // 10
 
 // Anonymous
+$quotient = function($n1, $n2) {
+    return $n1 / $n2 . '<br>';
+};
+echo $quotient(24, 6); // 4
+
+// Arrow
+$areaOfCircle = fn($radius) => $radius * $radius * 3.14;
+echo $areaOfCircle(4); // 50.24
